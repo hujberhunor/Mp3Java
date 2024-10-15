c439a4b5b0e5e6e3398983ca61dc5ceec16fea2b
Hujber Hunor <hujber.hunor@gmail.com>
2024-10-15 16:58:15 +0200

    FileHandler and Track class + mp3agic library added

---
+++ asd/demo/src/main/java/com/example/FileHandler.java
@@ -0,0 +1,81 @@
+package com.example;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import com.mpatric.mp3agic.ID3v1;
+import com.mpatric.mp3agic.ID3v2;
+import com.mpatric.mp3agic.InvalidDataException;
+import com.mpatric.mp3agic.Mp3File;
+import com.mpatric.mp3agic.UnsupportedTagException;
+
+/**
+ * FileHandler
+ */
+public class FileHandler {
+    // Ebben a kollekcióban  tárolom a dalokat
+    public ArrayList<Track> trackList = new ArrayList<>();
+  
+   /*
+   * @param path dal elérési útvonala
+   * A megadott dir végigpásztázása mp3 fájlok után kutatva 
+   * A megtalált mp3 fájlokból egyesével lértehoz egy T //rack objektumot 
+   */ 
+    public void readDir(String path) throws IOException, UnsupportedTagException,
+     InvalidDataException{
+        File dir = new File(path);
+        File[] listOfFiles = dir.listFiles();
+        String name;
+        
+        if(listOfFiles != null){
+            for(int i = 0; i < listOfFiles.length; i++){
+                name = listOfFiles[i].getName();
+
+                if(listOfFiles[i].isFile() && name.endsWith(".mp3")){
+                    Track track = createTrackFromPath(listOfFiles[i].getAbsolutePath());
+                    trackList.add(track);
+                }
+            } // end of for loop
+        } 
+        // Error handling
+        else System.out.println("Hiba a mappa pásztázásakor");
+    } // end of readDir() 
+
+
+    /*
+     * @param path - a dal elérési útvonala 
+     * Megadott útvonalból létrehozza a Track objektumot amit utána el tudok 
+     * tárolni egy kollekcióban. 
+     */
+     public Track createTrackFromPath(String path) throws IOException, UnsupportedTagException,
+     InvalidDataException {
+        // Inicializálás 
+        Mp3File mp3File = new Mp3File(path);
+        long length = 0;
+        String artist = null;
+        String title = null;
+        String album = null; 
+       
+        // Megnézem, hogy milyen típusú tagjei vannak
+        if(mp3File.hasId3v1Tag()){
+            ID3v1 v1Tag = mp3File.getId3v1Tag();
+            length = mp3File.getLengthInSeconds();
+            artist = v1Tag.getArtist();
+            title = v1Tag.getTitle();
+            album = v1Tag.getAlbum();
+        }
+        else if(mp3File.hasId3v2Tag()){
+            ID3v2 v2Tag = mp3File.getId3v2Tag();
+            length = mp3File.getLengthInSeconds();
+            artist = v2Tag.getArtist();
+            title = v2Tag.getTitle();
+            album = v2Tag.getAlbum();
+        }
+        else System.err.println("Se v1 se v2 tagjei nincsenek a fájlnak.");
+        
+        // Trakc konstruálás, lényegi rész
+        return new Track(path, title, artist, album, length);
+    } // end of createTrackFromPath()
+    
+} // end of fileHandler class
--- asd/demo/src/main/java/com/example/Track.java
+++ asd/demo/src/main/java/com/example/Track.java
@@ -1,5 +1,57 @@
 package com.example;
 
 public class Track {
-    
-}
+    String path;    // Absolute path that is set by the FileHandler.readDir()
+    String title;
+    String artis;
+    String album;
+    long length;    // Sec-ben megadott dal hossz 
+
+    // Konstruktor
+    Track(String path, String title, String artist, String album, long length){
+        this.path = path;
+        this.title = title;
+        this.artis = artist;
+        this.album = album;
+        this.length = length;
+    }
+
+    // Paraméter nélküli konstruktor
+    Track(){
+        this.path = null;
+        this.title = null;
+        this.artis = null;
+        this.album = null;
+        this.length = 0;
+    }
+
+    // Getterek
+
+    /*
+     * @returns path absolute path to the song 
+     * The fileHandler class Readdi method sets this attribute
+     */
+    public String getPath(){
+        return path;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public String getArtist(){
+        return artis;
+    }
+
+    public String album(){
+        return album;
+    }
+
+    public long getLength(){
+        return length;
+    }
+
+
+} // end of Track class
+
+
